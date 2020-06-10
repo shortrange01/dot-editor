3449d02364aff71cbc886ea9f374b2bf32d8f50c
shortrange01 <shortrange01@gmail.com>
2020-06-11 01:48:12 +0900

    mod: 諸々実装
    Signed-off-by: shortrange01 <shortrange01@gmail.com>

--- src/components/DotEditor.vue
+++ src/components/DotEditor.vue
@@ -1,132 +1,145 @@
 <template>
-    <div>
+    <div class="container dotEditor">
         <div class="editorBlock">
             <div class="editorGrid">
-                <div v-for="(dotLine, dotLineIndex) in dotList" :key="dotLineIndex" class="row">
-                    <div v-for="(dot, dotIndex) in dotLine" :key="dotIndex" @mousedown="onClickDot(dot)" @mouseover="onOverDot(dot)" class="col" :style="{backgroundColor: dot.color}"></div>
+                <div
+                    v-for="(dotLine, dotLineIndex) in dotList"
+                    :key="dotLineIndex"
+                    class="row"
+                >
+                    <div
+                        v-for="(dot, dotIndex) in dotLine"
+                        :key="dotIndex"
+                        @mousedown="onClickDot(dot)"
+                        @mouseover="onOverDot(dot)"
+                        class="col"
+                        :style="{ backgroundColor: dot.color }"
+                    ></div>
                 </div>
             </div>
             <chrome-picker v-model="nowColor" />
         </div>
         <div class="colorHistoryBrock">
-            <div v-for="(color, colorIndex) in colorHistory" :key="colorIndex" @click="onClickColorHistory(color)" :style="{backgroundColor: color}" class="colorHistory"></div>
+            <div
+                v-for="(color, colorIndex) in colorHistory"
+                :key="colorIndex"
+                @click="onClickColorHistory(color)"
+                :style="{ backgroundColor: color }"
+                class="colorHistory"
+                :class="{ active: !isEraserMode }"
+            ></div>
+            <div
+                class="eraser"
+                :class="{ active: isEraserMode }"
+                @click="onClickEraser()"
+            >
+                <div class="left"></div>
+                <div class="right">
+                    <div class="top"><p>PLASTIC ERASER</p></div>
+                    <div class="middle"><p>MONO</p></div>
+                    <div class="bottom"><p>TOMBO</p></div>
+                </div>
+            </div>
         </div>
         <div class="btn download" @click="onClickDownLoad">download</div>
-        <input @change="onChangeFile" type="file" />
+        <div class="btn load" @click="onClickLoadBtn">load json</div>
+        <input
+            ref="inputFile"
+            style="display: none;"
+            @change="onChangeFile"
+            type="file"
+            value=""
+        />
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import {Chrome} from 'vue-color';
+import { Component, Watch, Vue } from "vue-property-decorator";
+import { Chrome } from "vue-color";
 
-type dotList = [{color: string}[]?];
+type dotList = [{ color: string }[]?];
 
 @Component({
     components: {
-     chromePicker:Chrome
-    }
+        chromePicker: Chrome,
+    },
 })
 export default class DotEditor extends Vue {
-//   @Prop() private msg!: string;
-    dotList:dotList = [];
-    colorHistory:string[] = [];
-    nowColor = {hex:'#000'};
-    
+    dotList: dotList = [];
+    colorHistory: string[] = [];
+    nowColor = { hex: "#000" };
+    isEraserMode = false;
+    inputFileElement?: HTMLInputElement;
+
     created() {
-        for(let i = 1; i <= 16; i++ ){
+        for (let i = 1; i <= 16; i++) {
             const arr = [];
-            for(let i = 1; i <= 16; i++ ){
-                arr.push({color: ''});
+            for (let i = 1; i <= 16; i++) {
+                arr.push({ color: "" });
             }
             this.dotList.push(arr);
         }
     }
-    changeColor(dot: {color: string}){
+    mounted() {
+        this.inputFileElement = this.$refs.inputFile as HTMLInputElement;
+    }
+    changeColor(dot: { color: string }) {
+        if (this.isEraserMode) {
+            dot.color = "";
+            return;
+        }
         dot.color = this.nowColor.hex;
-        if(this.colorHistory.indexOf(dot.color) < 0) {
+        if (this.colorHistory.indexOf(dot.color) < 0) {
             this.colorHistory.push(dot.color);
-            if(this.colorHistory.length > 20) {
+            if (this.colorHistory.length > 17) {
                 this.colorHistory.shift();
             }
         }
     }
-    onClickDot(dot: {color: string}):void {
+    onClickDot(dot: { color: string }): void {
         this.changeColor(dot);
     }
-    onOverDot(dot: {color: string}):void {
+    onOverDot(dot: { color: string }): void {
         this.changeColor(dot);
     }
-    onClickColorHistory(color: string):void {
-        this.nowColor = {hex: color};
+    onClickColorHistory(color: string): void {
+        this.isEraserMode = false;
+        this.nowColor = { hex: color };
     }
-    onClickDownLoad():void {
-        const link:HTMLAnchorElement = document.createElement("a");
-        const data:{dotList: dotList, colorHistory: string[]} = {dotList: this.dotList, colorHistory: this.colorHistory};
-        link.href = "data:text/plain," + encodeURIComponent(JSON.stringify(data));
-        link.download = `${Math.round((new Date()).getTime() / 1000)}.json`;
+    onClickEraser() {
+        this.isEraserMode = !this.isEraserMode;
+    }
+    onClickDownLoad(): void {
+        const link: HTMLAnchorElement = document.createElement("a");
+        const data: { dotList: dotList; colorHistory: string[] } = {
+            dotList: this.dotList,
+            colorHistory: this.colorHistory,
+        };
+        link.href =
+            "data:text/plain," + encodeURIComponent(JSON.stringify(data));
+        link.download = `${Math.round(new Date().getTime() / 1000)}.json`;
         link.click();
     }
-    onChangeFile(event):void {
-        console.info(event);
-        const reader = new FileReader();
-        reader.readAsText(event.currentTarget.files[0]);
-        reader.addEventListener("load", (event:any) => {
-            const result = JSON.parse(event.currentTarget.result);
-            this.dotList = result.dotList;
-            this.colorHistory = result.colorHistory;
-        });
+    onChangeFile(event: any): void {
+        if (event.currentTarget.value) {
+            const reader: FileReader = new FileReader();
+            reader.readAsText(event.currentTarget.files[0]);
+            reader.addEventListener("load", (event: any) => {
+                // value を消さないと同じファイルを連続で読み込む際に change event が発火しない
+                if (this.inputFileElement) this.inputFileElement.value = "";
+                const result = JSON.parse(event.currentTarget.result);
+                this.dotList = result.dotList;
+                this.colorHistory = result.colorHistory;
+            });
+        }
+    }
+    onClickLoadBtn() {
+        if (this.inputFileElement) this.inputFileElement.click();
+    }
+
+    @Watch("nowColor.hex")
+    onChangeNewColor() {
+        this.isEraserMode = false;
     }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-div {
-    box-sizing: border-box;
-}
-.editorBlock {
-    display: flex;
-}
-.editorGrid {
-    box-sizing:content-box;
-    width: 256px;
-    height: 256px;
-    border: solid 2px #999;
-    margin-right: 10px;
-}
-.row {
-    width: 256px;
-    height: 16px;
-    display: flex;
-    border-bottom: solid 1px #ccc;
-}
-.col {
-    width: 16px;
-    border-left: solid 1px #ccc;
-}
-.colorHistoryBrock {
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: 10px;
-    width: 500px;
-}
-.colorHistory {
-    width: 46px;
-    height: 46px;
-    border-radius: 23px;
-    margin-bottom: 4px;
-    box-shadow: 4px 4px 4px -1px rgba(0,0,0,0.4) inset;
-    margin-left: 4px;
-}
-.btn {
-    padding: 10px;
-    border: solid 1px #c3c;
-    color: #c3c;
-    border-radius: 10px;
-}
-.btn.download {
-    width: 500px;
-    margin: 10px auto;
-}
-</style>
