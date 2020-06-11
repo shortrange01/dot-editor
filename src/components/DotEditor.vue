8851a2f699fc58007049bfaa8e924a561cbf5ac4
shortrange01 <shortrange01@gmail.com>
2020-06-11 14:33:17 +0900

    add: canvasプレビュー&pngダウンロード color-pickerセレクト追加
    描いたものをcanvas上でリアルタイムに描画し、
    pngファイルとしてダウンロードができる。
    
    vue-colorのpickerの種類を切り替えることができる。
    ボタン押下によるdynamic-componentで切り替えを行う。

--- src/components/DotEditor.vue
+++ src/components/DotEditor.vue
@@ -1,67 +1,90 @@
 <template>
     <div class="container dotEditor">
-        <div class="editorBlock">
-            <div class="editorGrid">
-                <div
-                    v-for="(dotLine, dotLineIndex) in dotList"
-                    :key="dotLineIndex"
-                    class="row"
-                >
+        <canvas id="canvas" class="canvas" width="512" height="512"></canvas>
+        <div>
+            <div class="editorBlock">
+                <div class="editorGrid">
                     <div
-                        v-for="(dot, dotIndex) in dotLine"
-                        :key="dotIndex"
-                        @mousedown="onClickDot(dot)"
-                        @mouseover="onOverDot(dot)"
-                        class="col"
-                        :style="{ backgroundColor: dot.color }"
-                    ></div>
+                        v-for="(dotLine, dotLineIndex) in dotList"
+                        :key="dotLineIndex"
+                        class="row"
+                    >
+                        <div
+                            v-for="(dot, dotIndex) in dotLine"
+                            :key="dotIndex"
+                            @mousedown="onClickDot(dot)"
+                            @mouseover="onOverDot(dot)"
+                            class="col"
+                            :style="{ backgroundColor: dot.color }"
+                        ></div>
+                    </div>
+                </div>
+                <div class="pickerWrap">
+                    <component :is="pickerComponent" v-model="nowColor" />
                 </div>
             </div>
-            <chrome-picker v-model="nowColor" />
-        </div>
-        <div class="colorHistoryBrock">
-            <div
-                v-for="(color, colorIndex) in colorHistory"
-                :key="colorIndex"
-                @click="onClickColorHistory(color)"
-                :style="{ backgroundColor: color }"
-                class="colorHistory"
-                :class="{ active: !isEraserMode }"
-            ></div>
-            <div
-                class="eraser"
-                :class="{ active: isEraserMode }"
-                @click="onClickEraser()"
-            >
-                <div class="left"></div>
-                <div class="right">
-                    <div class="top"><p>PLASTIC ERASER</p></div>
-                    <div class="middle"><p>MONO</p></div>
-                    <div class="bottom"><p>TOMBO</p></div>
+            <div class="pickerBtnBrock">
+                <div class="btn" @click="pickerComponent = 'ChromePicker'">Chrome</div>
+                <div class="btn" @click="pickerComponent = 'PhotoshopPicker'">Photoshop</div>
+                <div class="btn" @click="pickerComponent = 'MaterialPicker'">Material</div>
+                <div class="btn" @click="pickerComponent = 'CompactPicker'">Compact</div>
+                <div class="btn" @click="pickerComponent = 'SwatchesPicker'">Swatches</div>
+                <div class="btn" @click="pickerComponent = 'SliderPicker'">Slider</div>
+                <div class="btn" @click="pickerComponent = 'SketchPicker'">Sketch</div>
+            </div>
+            <div class="colorHistoryBrock">
+                <div
+                    v-for="(color, colorIndex) in colorHistory"
+                    :key="colorIndex"
+                    @click="onClickColorHistory(color)"
+                    :style="{ backgroundColor: color }"
+                    class="colorHistory"
+                    :class="{ active: !isEraserMode }"
+                ></div>
+                <div
+                    class="eraser"
+                    :class="{ active: isEraserMode }"
+                    @click="onClickEraser()"
+                >
+                    <div class="left"></div>
+                    <div class="right">
+                        <div class="top"><p>PLASTIC ERASER</p></div>
+                        <div class="middle"><p>MONO</p></div>
+                        <div class="bottom"><p>TOMBO</p></div>
+                    </div>
                 </div>
             </div>
+            <div class="fileBtnBrock">
+                <div class="btn" @click="onClickDownLoad">download</div>
+                <div class="btn" @click="onClickLoadBtn">load json</div>
+                <div class="btn" @click="onClickPingDownLoad">png download</div>
+            </div>
+            <input
+                ref="inputFile"
+                style="display: none;"
+                @change="onChangeFile"
+                type="file"
+                value=""
+            />
         </div>
-        <div class="btn download" @click="onClickDownLoad">download</div>
-        <div class="btn load" @click="onClickLoadBtn">load json</div>
-        <input
-            ref="inputFile"
-            style="display: none;"
-            @change="onChangeFile"
-            type="file"
-            value=""
-        />
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Watch, Vue } from "vue-property-decorator";
-import { Chrome } from "vue-color";
+import { Chrome, Photoshop, Material, Compact, Swatches, Slider, Sketch } from "vue-color";
 
 type dotList = [{ color: string }[]?];
 
 @Component({
     components: {
-        chromePicker: Chrome,
+        ChromePicker: Chrome,
+        PhotoshopPicker: Photoshop,
+        MaterialPicker: Material,
+        CompactPicker: Compact,
+        SwatchesPicker: Swatches,
+        SliderPicker: Slider,
+        SketchPicker: Sketch
     },
 })
 export default class DotEditor extends Vue {
@@ -70,6 +93,8 @@ export default class DotEditor extends Vue {
     nowColor = { hex: "#000" };
     isEraserMode = false;
     inputFileElement?: HTMLInputElement;
+    pickerComponent = "ChromePicker";
+    canvas?:HTMLCanvasElement;
 
     created() {
         for (let i = 1; i <= 16; i++) {
@@ -82,13 +107,16 @@ export default class DotEditor extends Vue {
     }
     mounted() {
         this.inputFileElement = this.$refs.inputFile as HTMLInputElement;
+        this.canvas = document.querySelector('#canvas') as HTMLCanvasElement;
     }
     changeColor(dot: { color: string }) {
         if (this.isEraserMode) {
             dot.color = "";
+            this.drowCanvas();
             return;
         }
         dot.color = this.nowColor.hex;
+        this.drowCanvas();
         if (this.colorHistory.indexOf(dot.color) < 0) {
             this.colorHistory.push(dot.color);
             if (this.colorHistory.length > 17) {
@@ -120,6 +148,12 @@ export default class DotEditor extends Vue {
         link.download = `${Math.round(new Date().getTime() / 1000)}.json`;
         link.click();
     }
+    onClickPingDownLoad(): void {
+        let link:HTMLAnchorElement = document.createElement("a");
+        link.href = this.canvas ? this.canvas.toDataURL("image/png") : "";
+        link.download = `${Math.round(new Date().getTime() / 1000)}.png`;
+        link.click();
+    }
     onChangeFile(event: any): void {
         if (event.currentTarget.value) {
             const reader: FileReader = new FileReader();
@@ -130,11 +164,27 @@ export default class DotEditor extends Vue {
                 const result = JSON.parse(event.currentTarget.result);
                 this.dotList = result.dotList;
                 this.colorHistory = result.colorHistory;
+                this.drowCanvas();
             });
         }
     }
     onClickLoadBtn() {
         if (this.inputFileElement) this.inputFileElement.click();
+    }
+    drowCanvas() {
+        // const canvas:HTMLCanvasElement | null = document.querySelector('#canvas');
+        let ctx:CanvasRenderingContext2D | null = this.canvas ? this.canvas.getContext('2d') : null;
+        if(ctx !== null && this.canvas) ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.dotList.forEach((list, listIndex) => {
+            if(list){
+                list.forEach((val, valIndex) => {
+                    if(val.color && ctx !== null) {
+                        ctx.fillStyle = val.color;
+                        ctx.fillRect(valIndex*32, listIndex*32, 32, 32);
+                    }
+                });
+            }
+        });
     }
 
     @Watch("nowColor.hex")
